a3b4264d6ba9c2e0e13fd51a83567a24b55c0d08
rutiksanap44 <sanaprutik1@gmail.com>
2023-07-15 22:43:00 +0530

    Upper case feature added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import 
 import './App.css';
 import Navbar from './Components/Navbar';
+import Uppercase from './Components/Uppercase';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Uppercase></Uppercase>
     </div>
   );
 }
---
+++ src/Components/Uppercase.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+export default function Uppercase(props) {
+
+    const [text,Newtext] = useState("This was text")
+    
+    function handleOnChange(event){
+        Newtext(event.target.value)
+    }
+    function handleOnClick(){
+        var upperCaseText = text.toUpperCase()
+        Newtext(upperCaseText)
+    }
+    return (
+        <div>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="exampleFormControlTextarea1">Example textarea</label>
+                    <br></br>
+                    <textarea className="form-control my-5" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <br></br>
+                    <button type="button" className="btn btn-primary" onClick={handleOnClick}>Convert to Upper Case</button>
+                </div>
+            </form>
+            <h4>Number of letters : {text.length}</h4>
+            <h4>Number of Words : {text.split(" ").length}</h4>
+        </div>
+    )
+}
